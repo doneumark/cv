4efbd73d57d81d74db33c2e30bb823255005c640
Don Neuamrk <unwis1@gmail.com>
2022-12-28 00:17:59 +0200

    cleaned client pages

--- packages/client/src/app/app.tsx
+++ packages/client/src/app/app.tsx
@@ -1,47 +1,34 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import AboutPage from './pages/AboutPage';
+import GeneratePage from './pages/GeneratePage';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="client" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/generate">Generate</Link>
           </li>
           <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/about">About</Link>
           </li>
         </ul>
       </div>
       <Routes>
         <Route
-          path="/"
+          path="/generate"
           element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
+            <GeneratePage title='generate' />
           }
         />
         <Route
-          path="/page-2"
+          path="/about"
           element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
+            <AboutPage title='about' />
           }
         />
       </Routes>
