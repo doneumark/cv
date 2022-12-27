6a23410e33ac27c21c1d8a02e33b1a92c47473c3
Don Neuamrk <unwis1@gmail.com>
2022-12-28 00:17:47 +0200

    updated api

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -5,12 +5,12 @@ export default {
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
-    }
+    },
   },
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest'
+    '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/api'
+  coverageDirectory: '../../coverage/packages/api',
 };
