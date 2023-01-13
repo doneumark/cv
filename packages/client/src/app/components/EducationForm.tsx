1d211ff74d6a1165273c05a52a41b8c297fdc7e6
Don Neuamrk <unwis1@gmail.com>
2023-01-13 15:18:34 +0200

    added education

---
+++ packages/client/src/app/components/EducationForm.tsx
@@ -0,0 +1,133 @@
+import { Education } from '@cv/api/interface';
+import { useForm } from 'react-hook-form';
+import { useQueryClient } from 'react-query';
+import { useEffect, useState } from 'react';
+import * as api from '../services/api';
+import Button from './Button';
+import Input from './Input';
+import Label from './Label';
+import StartsAtEndsAtInput from './StartsAtEndsAtInput';
+import OutlineSaveIcon from '../icons/OutlineSaveIcon';
+import { useToast } from '../services/toasts';
+
+export interface EducationFormProps {
+	education?: Education;
+	onClose: () => void;
+}
+
+export default function EducationForm({ education, onClose }: EducationFormProps) {
+	const {
+		register,
+		handleSubmit,
+		formState: { isDirty, isSubmitting },
+		reset,
+		control,
+	} = useForm({ defaultValues: education });
+
+	const queryClient = useQueryClient();
+	const { addToast } = useToast();
+	const [isDeleting, setIsDeleting] = useState(false);
+
+	useEffect(() => reset(education), [education, reset]);
+
+	const save = async (data: Education) => {
+		try {
+			if (!education) {
+				await api.createEducation(data);
+				queryClient.invalidateQueries(['educations']);
+				queryClient.invalidateQueries(['userCounts']);
+				addToast({ message: 'Education created successfully', type: 'success' });
+			} else {
+				await api.updateEducation(education.id, data);
+				queryClient.invalidateQueries(['educations']);
+				addToast({ message: 'Education updated successfully', type: 'success' });
+			}
+
+			if (onClose) {
+				onClose();
+			}
+
+			reset(data);
+		} catch {
+			addToast({ message: 'Education save failed', type: 'error' });
+		}
+	};
+
+	const onClickDelete = async () => {
+		if (!education) {
+			return;
+		}
+
+		setIsDeleting(true);
+
+		try {
+			await api.deleteEducation(education.id);
+			queryClient.invalidateQueries(['educations']);
+			queryClient.invalidateQueries(['userCounts']);
+			addToast({ message: 'Education deleted successfully', type: 'info' });
+
+			if (onClose) {
+				onClose();
+			}
+
+			reset();
+		} catch (err) {
+			addToast({ message: 'Education delete failed', type: 'error' });
+		}
+
+		setIsDeleting(false);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(save)} className='space-y-6'>
+			<div className='form-control'>
+				<Label text='School' />
+				<Input type='text' placeholder='School Name' {...register('school')} />
+			</div>
+			<div className='form-control'>
+				<Label text='Degree' />
+				<Input type='text' placeholder='Degree Name' {...register('degreeName')} />
+			</div>
+			<div className='form-control'>
+				<Label text='Field of study' />
+				<Input type='text' placeholder='Field Name' {...register('field')} />
+			</div>
+			<StartsAtEndsAtInput control={control} />
+			<div className='form-control'>
+				<Label text='Grade' />
+				<Input
+					type='number'
+					min={0}
+					max={100}
+					placeholder='Grade'
+					{...register('grade', {
+						valueAsNumber: true,
+					})}
+				/>
+			</div>
+			<div className='form-control'>
+				<Label text='Description' />
+				<textarea
+					className='textarea textarea-bordered block'
+					rows={2}
+					placeholder='Description'
+					{...register('description')}
+				/>
+			</div>
+			<div className='flex justify-between gap-2'>
+				<div>
+					{ education && <Button color='secondary' type='button' onClick={onClickDelete} loading={isDeleting}>Delete</Button> }
+				</div>
+				<div className='flex gap-2'>
+					<Button outline color='secondary' type='button' onClick={onClose}>
+						Cancel
+					</Button>
+					<Button className='gap-2' loading={isSubmitting} color='secondary' type='submit' disabled={!isDirty}>
+						{ !isSubmitting && <OutlineSaveIcon /> }
+						Save
+					</Button>
+				</div>
+			</div>
+		</form>
+	);
+}
--- packages/client/src/app/pages/EducationsPage.tsx
+++ packages/client/src/app/pages/EducationsPage.tsx
@@ -1,16 +1,102 @@
 import { useState, useMemo } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { useQuery } from 'react-query';
-import Table from '../components/Table';
-import Button from '../components/Button';
-import PageTitle from '../components/PageTitle';
-import PageContent from '../components/PageContent';
+import { Education } from '@cv/api/interface';
 import * as api from '../services/api';
+import Button from '../components/Button';
 import { parseApiDate, filterByQuery } from '../services/misc';
-
 import SearchInput from '../components/SearchInput';
-import Spinner from '../components/Spinner';
+import Modal from '../components/Modal';
+import EducationForm from '../components/EducationForm';
+import PageContent from '../components/PageContent';
+import PageTitle from '../components/PageTitle';
+import PlusIcon from '../icons/PlusIcon';
+import PencilIcon from '../icons/PencilIcon';
+import LoadingContainer from '../components/LoadingContainer';
+import { useFormRoute } from '../services/routes';
+
+interface EducationBoxProps {
+	education: Education;
+}
+
+function EducationBox({ education }: EducationBoxProps) {
+	return (
+		<Link className='card card-bordered border-base-300 card-compact hover:shadow-md cursor-pointer flex items-between group' to={education.id}>
+			<div className='card-body flex-row items-center justify-between'>
+				<div>
+					<div className='flex items-center gap-3'>
+						<div className='card-title'>
+							{ education.school }
+						</div>
+						<h6>{ `${education.degreeName}, ${education.field}` }</h6>
+					</div>
+					<div className='flex items-center gap-3'>
+						<div className='flex items-center'>
+							{ parseApiDate(
+								education.startsAtDay,
+								education.startsAtMonth,
+								education.startsAtYear,
+							) }
+							{ ' - ' }
+							{ parseApiDate(
+								education.endsAtDay,
+								education.endsAtMonth,
+								education.endsAtYear,
+							) || 'Now' }
+						</div>
+						{ `${education.description}${education.grade ? ` (Grade: ${education.grade})` : ''}` }
+					</div>
+				</div>
+				<div className='hidden group-hover:block'>
+					<PencilIcon />
+				</div>
+			</div>
+		</Link>
+	);
+}
+
+function CreateEducationModal() {
+	const { isCreatePath, rootPath: educationsPath } = useFormRoute();
+	const navigate = useNavigate();
+
+	return (
+		<Modal show={isCreatePath} onClose={() => navigate(educationsPath)}>
+			<div className='prose mb-6'>
+				<h3>Create Education</h3>
+			</div>
+			<EducationForm onClose={() => navigate(educationsPath)} />
+		</Modal>
+	);
+}
+
+function UpdateEducationModal() {
+	const { isUpdatePath, pathParam: educationId, rootPath: educationsPath } = useFormRoute();
+	const navigate = useNavigate();
+
+	const { data: education, isLoading } = useQuery({
+		queryKey: ['education', educationId],
+		queryFn: () => (educationId ? api.getEducation(educationId) : null),
+		enabled: isUpdatePath,
+		refetchOnWindowFocus: false,
+	});
+
+	return (
+		<Modal show={isUpdatePath} onClose={() => navigate(educationsPath)}>
+			<div className='prose mb-6'>
+				<h3>Update Education</h3>
+			</div>
+			<LoadingContainer height={400} isLoading={isLoading}>
+				<EducationForm
+					education={education || undefined}
+					onClose={() => navigate(educationsPath)}
+				/>
+			</LoadingContainer>
+		</Modal>
+	);
+}
 
 export default function Educations() {
+	const navigate = useNavigate();
 	const [search, setSearch] = useState('');
 	const { data: educations, isLoading } = useQuery({
 		queryKey: ['educations'],
@@ -26,71 +112,31 @@ export default function Educations() {
 		<>
 			<PageTitle title='Educations' />
 			<PageContent>
-				{ isLoading
-					? (
-						<div className='w-full h-60'>
-							<Spinner />
+				<LoadingContainer height={200} isLoading={isLoading}>
+					<div className='space-y-6'>
+						<div className='flex justify-between'>
+							<SearchInput value={search} onChange={(e) => setSearch(e.target.value)} />
+							<Button size='sm' color='secondary' type='submit' className='gap-2' onClick={() => navigate('new')}>
+								<PlusIcon />
+								Add Education
+							</Button>
 						</div>
-					)
-					: (
-						<div className='space-y-6'>
-							<div className='flex justify-between'>
-								<SearchInput value={search} onChange={(e) => setSearch(e.target.value)} />
-								<Button size='sm' color='secondary' type='submit' className='gap-2'>
-									<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
-										<path strokeLinecap='round' strokeLinejoin='round' d='M12 4.5v15m7.5-7.5h-15' />
-									</svg>
-									Add Education
-								</Button>
-							</div>
-							<Table>
-								<thead>
-									<tr>
-										<th>From</th>
-										<th>To</th>
-										<th>School</th>
-										<th>Degree</th>
-										<th>Field of Study</th>
-										<th>Grade</th>
-										<th>Description</th>
-									</tr>
-								</thead>
-								<tbody>
-									{ !filteredEducations.length && (
-										<tr>
-											<td colSpan={7} className='text-center'>
-												No education found
-											</td>
-										</tr>
-									)}
-									{ filteredEducations.map((education) => (
-										<tr key={education.id}>
-											<td>
-												{ parseApiDate(
-													education.startsAtDay,
-													education.startsAtMonth,
-													education.startsAtYear,
-												) }
-											</td>
-											<td>
-												{ parseApiDate(
-													education.endsAtDay,
-													education.endsAtMonth,
-													education.endsAtYear,
-												) }
-											</td>
-											<td>{ education.school }</td>
-											<td>{ education.degreeName }</td>
-											<td>{ education.field }</td>
-											<td>{ education.grade }</td>
-											<td>{ education.description }</td>
-										</tr>
-									)) }
-								</tbody>
-							</Table>
+
+						<div className='space-y-3'>
+							{ filteredEducations.map((education) => (
+								<EducationBox education={education} key={`education-box-${education.id}`} />
+							)) }
+							{ !filteredEducations.length && (
+								<div className='text-center'>
+									No educations added yet
+								</div>
+							)}
 						</div>
-					)}
+					</div>
+				</LoadingContainer>
 			</PageContent>
+			<CreateEducationModal />
+			<UpdateEducationModal />
 		</>
 	);
 }
